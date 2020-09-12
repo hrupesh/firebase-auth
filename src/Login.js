25c65de8246149929400a2c222fd604a051e928c
hrupesh <rcashok0@gmail.com>
2020-09-13 04:17:56 +0530

    Auto Commit 😁

---
+++ src/Login.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Login() {
+  return (
+    <View>
+      <Text>This is LOGIN</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
