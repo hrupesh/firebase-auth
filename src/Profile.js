74028bbf6bbffcc03646f28cbcbc1cabbe6d6c90
hrupesh <rcashok0@gmail.com>
2020-09-14 02:55:40 +0530

    Auto Commit 😁

--- src/Profile.js
+++ src/Profile.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function Profile() {
   return (
     <View>
-      <Text>This is Profile!</Text>
+      <Text>Profile</Text>
     </View>
   );
 }
