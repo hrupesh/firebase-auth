f99aa2ad906f551b527d3ead9660c8abdb7e11bb
hrupesh <rcashok0@gmail.com>
2020-09-19 16:55:34 +0530

    Auto Commit 😁

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import { NavigationContainer } from "@react-navigastion/native";
-import { createStackNavigator } from "@react-navigastion/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import Login from "./src/Login";
 import Profile from "./src/Profile";
 
@@ -11,6 +11,7 @@ const Stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
+      <StatusBar style="light" />
       <Stack.Navigator
         screenOptions={{
           headerStyle: {
--- src/Login.js
+++ src/Login.js
@@ -1,12 +1,44 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
+import { TextInput } from "react-native-gesture-handler";
 
 export default function Login() {
+  const [loading, setLoading] = useState(false);
+
   return (
-    <View>
-      <Text>LOGIN</Text>
+    <View style={styles.screen}>
+      {loading ? (
+        <View style={styles.container}>
+          <Image
+            style={styles.loader}
+            source={{
+              uri:
+                "https://i.pinimg.com/originals/f9/41/ae/f941ae9d16fd7d2957eea6e5b1100d1e.gif",
+            }}
+          />
+          <Text>Coming Soon....</Text>
+        </View>
+      ) : null}
+      <View>
+        <Text>Email:</Text>
+        <TextInput placeholder="Enter your email id" />
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100%",
+    backgroundColor: "#fff",
+  },
+  loader: {
+    height: 500,
+    width: 500,
+  },
+  screen:{
+    padding:10
+  }
+});
